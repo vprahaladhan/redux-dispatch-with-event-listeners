ab3f0b753ac1f2ca665fac9df7a11a17d728f840
prahal <vprahaladhan@gmail.com>
2021-06-25 21:41:14 +1000

    commit tree 2d0f73122720f1f1f37d1e4db7302c422fc89a78

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,26 @@
 // add code snippets from README
+let state
+
+const reducer = (state = {count: 0}, action) => {
+  switch(action.type) {
+    case 'INCREASE_COUNT': 
+      return {...state, count: state.count + 1}
+    default: 
+      return state
+  }
+}
+
+const dispatch = (action) => {
+  state = reducer(state, action)
+  render()
+}
+
+const render = () => {
+  document.getElementById('container').innerText = state.count
+}
+
+document.getElementById('button').onclick = () => {
+  dispatch({type: 'INCREASE_COUNT'})
+}
+
+dispatch({type: '@@INIT'})
